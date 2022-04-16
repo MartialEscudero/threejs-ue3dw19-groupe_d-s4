28137570fcaa05935db68cc9af16f0aea63db6c0
MartialEscudero <martial.escudero@gmail.com>
2022-04-16 22:12:16 +0200

    Add click on objects

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,8 +4,10 @@ import Stats from './stats.module.js';
 
 import { ColladaLoader } from './ColladaLoader.js';
 
-let container, stats, clock;
-let camera, scene, renderer, tower, f22;
+let container, stats, clock, camera, scene, renderer, tower, f22;
+
+const raycaster = new THREE.Raycaster();
+const pointer = new THREE.Vector2();
 
 init();
 animate();
@@ -13,6 +15,7 @@ animate();
 function init() {
 
   container = document.getElementById('container');
+
   // Camera initialization
   camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 2000);
   camera.position.set(300, 100, 100);
@@ -20,7 +23,6 @@ function init() {
 
   // Scene initialization with fog and background image
   scene = new THREE.Scene();
-
   clock = new THREE.Clock();
 
   // loading manager
@@ -33,15 +35,20 @@ function init() {
 
   // Load collada tower
   const loader = new ColladaLoader(loadingManager);
+
   loader.load('./assets/models/tower.dae', function (collada) {
 
     tower = collada.scene;
+    tower.name = 'tower';
+    tower.link = 'https://fr.wikipedia.org/wiki/Tower';
 
   });
 
   loader.load('./assets/models/f22.dae', function (collada) {
 
     f22 = collada.scene;
+    f22.name = 'f22';
+    f22.link = 'https://fr.wikipedia.org/wiki/Lockheed_Martin_F-22_Raptor';
 
   });
 
@@ -68,6 +75,32 @@ function init() {
   // Recalculate width and height en window resize
   window.addEventListener('resize', onWindowResize);
 
+  document.addEventListener('mousemove', onPointerMove);
+  document.addEventListener('click', onClick);
+
+}
+
+function onPointerMove(event) {
+
+	pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+	pointer.y =- (event.clientY / window.innerHeight) * 2 + 1;
+
+}
+
+// Add click on objects
+function onClick() {
+
+  raycaster.setFromCamera(pointer, camera);
+  
+  const intersects = raycaster.intersectObjects(scene.children, true);
+  
+  if (intersects.length > 0) {
+    if (!intersects[0].object.parent.parent.parent.link) {
+      window.open(intersects[0].object.parent.parent.parent.parent.link)
+    } else {
+      window.open(intersects[0].object.parent.parent.parent.link)
+    }
+  }
 }
 
 function onWindowResize() {
